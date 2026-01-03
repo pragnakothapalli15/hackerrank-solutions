6163e88bf5d918e02282daab1f069c622d2ca1bf
pragnakothapalli15 <pragnakothapalli15@gmail.com>
2026-01-03 21:21:44 +0530

    Add files via upload

---
+++ 48.py
@@ -0,0 +1,9 @@
+# Read input integers
+a = int(input().strip())
+b = int(input().strip())
+c = int(input().strip())
+d = int(input().strip())
+
+# Calculate and print the result
+result = a**b + c**d
+print(result)
